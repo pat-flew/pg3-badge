03bc8c8bcce6d014f46d545df4201280566f3150
PatrickF <patrick.frew@gmail.com>
2015-01-09 15:11:02 +0000

    Request badge permissions for ios 8

--- src/ios/Badge.m
+++ src/ios/Badge.m
@@ -11,8 +11,13 @@
 @implementation Badge
     - (void)setBadge:(CDVInvokedUrlCommand*)command {
 
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge categories:nil];
-        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        NSArray *vComp = [[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."];
+
+        if ([[vComp objectAtIndex:0] intValue] > 7) {
+            NSLog(@"Requesting notification permissions");
+            UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge categories:nil];
+            [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        }
 
         NSInteger index = [[command.arguments objectAtIndex:0] integerValue];
         [[UIApplication sharedApplication] setApplicationIconBadgeNumber:index];
